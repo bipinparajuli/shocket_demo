71b24bb5c42a0a306fcc23faa0a4968627d43ea6
bipinparajuli <bipinprjl@gmail.com>
2021-06-06 19:14:05 +0545

    adding typing function

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-const { log } = require("console");
-const express = require("express");
-const app =express()
-const http = require("http");
-const server = http.createServer(app);
-const {Server} = require("socket.io")
-
-const io = new Server(server);
-
-
+let express = require('express')
+let app = express()
+let socket = require('socket.io')
 
 app.get("/",(req,res)=>{
-res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + "/index.html")
+})
+
+let server= app.listen(8000,()=>{
+    console.log("Server is running at 4000");
 })
 
 
-io.on("connection",(socket)=>{
-    console.log("User is Conencted");
+// Socket setup & pass server
+var io = socket(server);
+
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
 
 
-    socket.on("chat messege",(msg)=>{
-        console.log("User disconnect",msg);
-        io.emit("chat messege",msg)
-    })
-
-})
-
-server.listen(8000,()=>{
-    console.log("App is running");
-})
